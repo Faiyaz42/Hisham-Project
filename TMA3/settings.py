6877e04d561bffbd164db7d927504d38a5c664a0
Faiyaz Ahmed <50289212+Faiyaz42@users.noreply.github.com>
2023-01-22 19:46:39 -0700

    'rest_framework',     'corsheaders',

--- TMA3/settings.py
+++ TMA3/settings.py
@@ -51,8 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
     'part1',
     'part2',
     'part3',
